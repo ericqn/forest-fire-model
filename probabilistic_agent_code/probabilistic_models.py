b7b89b835083b12eed3334a9bbaa2b3c0ffddc0c
ericqn <ericqn1234@gmail.com>
2025-03-15 16:24:18 -0700

    Added documentation for functions in probabilistic_models.py

--- probabilistic_agent_code/probabilistic_models.py
+++ probabilistic_agent_code/probabilistic_models.py
@@ -1,36 +1,25 @@
 import pandas as pd
 import numpy as np
 
-"""
-Given a dataframe, this function aims to calculate the conditional probability 
-of a given variable, x. 
-
-    Notable Paramters:
-    data : dataframe
-        A pd.dataframe that contains data such as the FFMC, DMC, DC, ISI and 
-        a boolean int representing if a fire is burning
-    
-    x : str
-        The name of the variable that is having the conditional variable found
-        for.
-
-    area : The name of the column that contains boolean ints representing if a 
-    fire is burning or not.
-
-    x_range: The number of bins used by the variable.
-
-    boolean: Determines if attempting to find P(x|area = 1) or P(x | area = 0).
-    When set to 1, the P(x|area = 1) is calculated for each x. Else, P(x|area = 0)
-    is calculated for each x.
-
-    target_var_count: The number of times the target variable has occured.
-
-    returns: a dictionary, where each key corresponds to a bin in data. The
-    corresponding value for a given key is the probability of 
-    P(x = key|area = boolean).
-"""
 
 def calculate_conditional_prob(data, x, area, x_range, boolean,target_var_count):
+    """
+    Given a dataframe, this function aims to calculate the conditional probability 
+    of a given variable, x. 
+
+    Args:
+        data (pd.DataFrame): A pd.dataframe that contains data such as the FFMC, DMC, DC, ISI and a boolean int 
+            representing if a fire is burning
+        x (str): The name of the variable that is having the conditional variable found for.
+        area (str): The name of the column that contains boolean ints representing if a fire is burning or not.
+        x_range (int): The number of bins used by the variable.
+        boolean (bool): Determines if attempting to find P(x|area = 1) or P(x | area = 0). When set to 1, the 
+            P(x|area = 1) is calculated for each x. Else, P(x|area = 0) is calculated for each x.
+        target_var_count (int): The number of times the target variable has occured.
+    Returns:
+        dict: Each key in the dict corresponds to a bin in data. The corresponding value for a given key is the 
+        probability of P(x = key|area = boolean).
+    """
     count_dictionary = {}
     probability_dictionary = {}
     conditional_prob_dictionary = {}
@@ -64,25 +53,31 @@ def calculate_lookup_table(conditional_variable, conditional_probabilty_list,tar
     targetKey = f"P({target_variable}|{conditional_variable} = 1, {conditional_variable} = 2, {conditional_variable} = 3,{conditional_variable} = 4, {conditional_variable} = 5)"
     temp_dict[targetKey] = final_prob
     return temp_dict
-"""
-This uses the Naive Bayes Assumption to estimate the parameters of a Bayes net.
 
-    Notable Parameters:
 
-    conditional_variables:list[int]
-        A list of int from 1-5, where each int represents the severity of a 
-        corresponding weather index. Index [0] corresponds to FFMC, Index[1] to
-        DMC, Index[2] to DC, and Index[3] to ISI
+def calculate_naive_bayes(
+        conditional_variables: list[int],
+        FFMC_condlist: dict,
+        DMC_condlist: dict,
+        DC_condlist: dict,
+        ISI_condlist: dict,
+        target_variable_prob: float
+    ):
+    """
+    This uses the Naive Bayes Assumption to estimate the parameters of a Bayes net.
 
-    FFMC_condlist, DMC_condlist, DC_condlist, ISI_condlist : dictionary
-        A dictionary corresponding to each of the weather indices. Each 
-        dictionary contains the conditional probability for a given severity.
+    Args:
+        conditional_variables (list[int]): A list of int from 1-5, where each int represents the severity of a corresponding weather index.
+            Index [0] corresponds to FFMC, Index [1] to DMC, Index [2] to DC, and Index [3] to ISI.
+        FFMC_condlist (dict): A dictionary corresponding to FFMC, containing the conditional probability for a given severity.
+        DMC_condlist (dict): A dictionary corresponding to DMC, containing the conditional probability for a given severity.
+        DC_condlist (dict): A dictionary corresponding to DC, containing the conditional probability for a given severity.
+        ISI_condlist (dict): A dictionary corresponding to ISI, containing the conditional probability for a given severity.
+        target_variable_prob (float): The probability that the target variable (a fire) has occurred.
 
-    target_variable_prob: float
-        The probability that the target variable (a fire) has occured.
-
-"""
-def calculate_naive_bayes(conditional_variables,FFMC_condlist,DMC_condlist,DC_condlist,ISI_condlist,target_variable_prob):
+    Returns:
+        float: The estimated conditional probability of the target variable (fire) given the conditional variables.
+    """
     FFMC = conditional_variables[0]
     DMC = conditional_variables[1]
     DC = conditional_variables[2]
@@ -96,22 +91,28 @@ def calculate_naive_bayes(conditional_variables,FFMC_condlist,DMC_condlist,DC_co
 
     return conditional_target_var
 
-
 def max_log_likelihood(
         fire: bool,
         indices: dict[str: list],
         data_array: np.array
 ):
     """
-    Given severity indices (ranging from 0-5) on FFMC, DMC, DC, ISI, this function aims to calculate the 
-    probability of a fire based on the indices using the maximum log likelihood algorithm
+    This function aims to calculate the probability of a fire based on the indices using the maximum log likelihood algorithm
+
+    Args:
+        fire (bool): Represents whether or not a fire has occurred or not
+        indices (dict): A dict where the keys the specified fire indices and the values are the severity values (from 0-5)
+        data_array (np.array): Obtained from utils.fire_dataloader()
+    
+    Returns:
+        probability (float): The estimated conditional probability of the target variable (fire) given the indices
 
     Example Usage:
-
-    indices = {'FFMC': [3, 4], 'DMC': [4, 5], 'DC': [3, 4, 5], 'ISI': [3, 4]} \\
-    fire_array = fire_dataloader('data/forestfires.csv', to_array=True) \\
-    max_log_likelihood(True, indices, fire_array) \\
-    --> 0.5356
+        indices = {'FFMC': [3, 4], 'DMC': [4, 5], 'DC': [3, 4, 5], 'ISI': [3, 4]}
+        fire_array = fire_dataloader('data/forestfires.csv', to_array=True)
+        
+        result = max_log_likelihood(True, indices, fire_array)
+        --> result = 0.5356
     """
     count_parents_and_fire = 0
     count_parents = 0
